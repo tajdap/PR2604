0093f4f2248660d4b86ec0dca7fab46eee9229f5
Patricija Videc <160044511+PatricijaVidec@users.noreply.github.com>
2026-06-01 10:45:39 +0200

    ime aplikacije

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="PR Nepremičnine",
+    page_title="Socioekonomski vplivi na rodnost",
 )
 
 st.write("# Pozdravljen na naši streamlit aplikaciji! 👋")
